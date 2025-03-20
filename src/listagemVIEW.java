179ffbac6c521f1355564ecdfc90fe206c9b0fa9
Hobgobers <luanthhoff@gmail.com>
2025-03-20 17:58:44 -0300

    Revert "Implementada navegação no botão Consultar Vendas"
    This reverts commit 506f9377f44f5e1d3f2eccc148b1fea5cdddbaa3.

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -150,7 +150,6 @@ public class listagemVIEW extends javax.swing.JFrame {
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
         vendasVIEW vendas = new vendasVIEW(); 
         vendas.setVisible(true);
-        // ops :D
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
